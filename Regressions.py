c8d6610a8d26c5e3fbb90e6f9a98178cbb5aec9a
Ricardo-Jaramillo <78094995+Ricardo-Jaramillo@users.noreply.github.com>
2023-06-22 20:48:23 -0700

    Plot simple regression with Dummy vars if passed

--- Regressions.py
+++ Regressions.py
@@ -21,8 +21,10 @@ rng = default_rng(122708692400277160069775657973126599887)
 
 class Regressions():
     def __init__(self) -> None:
-        pass
-
+        self.color = [
+                        '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf',  
+                        '#aec7e8', '#ffbb78', '#98df8a', '#ff9896', '#c5b0d5', '#c49c94', '#f7b6d2', '#c7c7c7', '#dbdb8d', '#9edae5'
+                    ]
 
     # n x m DataFrame. Specify the columns to transform
     def transform(self, type: str, dataset: pd.DataFrame, columns: list):
@@ -34,18 +36,31 @@ class Regressions():
                 data_copy[column] = np.log(dataset[column])
             # Map data 
             if type == 'map':
-                data_copy[column] = dataset[column].map({'Yes': 1, 'No': 0})
-
+                i = 0
+                # Iterate value replacement with each color
+                for val in dataset[column].unique():
+                    data_copy[column] = data_copy[column].replace(val, self.color[i])
+                    i += 1
+        
         return data_copy
     
 
-    def __plot_simple_regression(self, dataset, model_results, y_column, x_column, x_column_dummies, alpha):
-        # Plot x, y
+    # Plot a simple Regression with dummy variables if specified
+    def __plot_simple_regression(self, dataset, model_results, y_column, x_column, x_column_dummies, dummy_columns, alpha):
+        # Set scatter colors if dummies
+        if dummy_columns:
+            c = dataset[dummy_columns[0]]
+        else:
+            c = self.color[1]
+        
         y = dataset[y_column]
         x = dataset[x_column]
         
-        # fig, ax = plt.subplots()
-        fig = plt.scatter(x, y)
+        # Plot x, y
+        fig, ax = plt.subplots()
+        ax.set_prop_cycle(color=self.color)
+        
+        fig = ax.scatter(x, y, c=c)
         
         # Plot Regression looping for model (&) dummy model
         for model_type, model in model_results:
@@ -59,13 +74,13 @@ class Regressions():
                 yhat = params[0] + params[x_column]*dataset[x_column]
                 
                 # Plot regression line
-                fig = plt.plot(x, yhat, label='Regression line')
+                fig = ax.plot(x, yhat, label='Regression line')
 
                 # Plot confidence bounds
                 if alpha:
                     prstd, iv_l, iv_u = wls_prediction_std(model, alpha=alpha)
-                    fig = plt.plot(x, iv_u, 'r--')
-                    fig = plt.plot(x, iv_l, 'r--')
+                    fig = ax.plot(x, iv_u, 'r--', label='upper bound')
+                    fig = ax.plot(x, iv_l, 'r--', label='lower bound')
 
             # Dummy_model Regression Line Equations
             if model_type == 'Dummy':
@@ -74,53 +89,13 @@ class Regressions():
                     yhat_dummies = params[0] + params[column] + params[x_column]*dataset[x_column]
                     
                     # Plot each dummy Regression Line
-                    fig = plt.plot(x, yhat_dummies, label=column)
+                    fig = ax.plot(x, yhat_dummies, label=column)
             
         plt.legend(loc='best')
         plt.xlabel(x_column)
         plt.ylabel(y_column)
         plt.show()
         
-        return
-        # Get params from models
-        original_model_params = model_results[0].params
-        original_model_rsquared = model_results[0].rsquared
-
-        dummy_model_params = model_results[1].params
-        dummy_model_rsquared = model_results[1].rsquared
-        
-        # Original Regression Line Equation
-        yhat = original_model_params[0] + original_model_params[x_column]*dataset[x_column]
-
-        # Dummy_model Regression Line Equation
-        yhat_dummies = []
-        for column in dataset[x_column_dummies]:
-            a = dummy_model_params[0] + dummy_model_params[column] + dummy_model_params[x_column]*dataset[x_column]
-            yhat_dummies.append(a)
-
-        # Set x1 with the x variable to be plot
-        y = dataset[y_column]
-        x = dataset[x_column]
-
-        # fig, ax = plt.subplots()
-        fig = plt.scatter(x, y, color='blue')
-        
-        # Set regression line equations
-        fig = plt.plot(x, yhat, c='orange', label ='regression line')
-        fig = plt.plot(x, yhat_dummies[0], c='green', label ='regression line')
-        fig = plt.plot(x, yhat_dummies[1], c='red', label ='regression line')
-        
-        # Get confidence bounds
-        if alpha:
-            prstd, iv_l, iv_u = wls_prediction_std(model_results, alpha=alpha)
-            fig = plt.plot(x, iv_u, 'r--')
-            fig = plt.plot(x, iv_l, 'r--')
-        
-        plt.legend(loc='best')
-        plt.xlabel(x_column)
-        plt.ylabel(y_column)
-        plt.show()
-
 
     # n x 2 DataFrame (y, xn-1, xn)
     def linear_regression(self, dataset: pd.DataFrame, y_column: str, x_columns: list, alpha: float=0, dummy_columns: list=[]):
@@ -149,23 +124,29 @@ class Regressions():
         '''
         
         # Init Results list and dataset with dummies
+        aux = dataset[dummy_columns]
+        
         results = []
         dataset = pd.get_dummies(data=dataset, columns=dummy_columns, dtype=float)
 
+        x_columns_dummies = []
         for dummy in dummy_columns:
             # x_columns_plot = dataset.loc[:, [dummy not in item and y_column != item for item in dataset.columns]].columns.to_list()
             x_columns_dummies = dataset.loc[:, [dummy in item for item in dataset.columns]].columns.to_list()
-
-        # Formulate the original model
-        reg_exp = f'{y_column} ~ {" + ".join(x_columns)}' # 'price ~ body_style_hardtop + body_style_hatchback + body_style_sedan + body_style_wagon'
-        ols_model_results = smf.ols(formula=reg_exp, data=dataset).fit()
-        results.append(('Original', ols_model_results))
         
+        # Map dummy columns
+        dataset[dummy_columns] = self.transform('map', aux, dummy_columns)
+
         # Formulate the model with dummies
         if dummy_columns:
             reg_exp = f'{y_column} ~ {" + ".join(x_columns + x_columns_dummies)}' # 'price ~ body_style_hardtop + body_style_hatchback + body_style_sedan + body_style_wagon'
             ols_model_results = smf.ols(formula=reg_exp, data=dataset).fit()
             results.append(('Dummy', ols_model_results))
+        
+        # Formulate the original model
+        reg_exp = f'{y_column} ~ {" + ".join(x_columns)}' # 'price ~ body_style_hardtop + body_style_hatchback + body_style_sedan + body_style_wagon'
+        ols_model_results = smf.ols(formula=reg_exp, data=dataset).fit()
+        results.append(('Original', ols_model_results))
 
         # Get results
         for _, result in results:
@@ -173,10 +154,5 @@ class Regressions():
 
         # Plot if simple linear regression
         if len(x_columns) == 1:
-            self.__plot_simple_regression(dataset, results, y_column, x_columns[0], x_columns_dummies, alpha)
+            self.__plot_simple_regression(dataset, results, y_column, x_columns[0], x_columns_dummies, dummy_columns, alpha)
         
-    
-    # n x 3 dataset. Specify dummy column to transform/map
-    def dummy_variables(self, dataset: pd.DataFrame, dummy_column: str):
-        # Map dummy_column
-        new_dataset = self.transform('map', dataset, [dummy_column])
