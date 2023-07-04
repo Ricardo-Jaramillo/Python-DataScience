428b9dc1356a85c8969cb1ecb98ee0aab5ed8012
Ricardo-Jaramillo <78094995+Ricardo-Jaramillo@users.noreply.github.com>
2023-07-03 17:14:54 -0700

    Load training_data as npz file

--- DeepLearning.py
+++ DeepLearning.py
@@ -1,6 +1,7 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 
 class DeepLearning:
--- main_deepLearning.py
+++ main_deepLearning.py
@@ -1,6 +1,7 @@
 from SQLServer import SQLServer
 from DeepLearning import DeepLearning
 import pandas as pd
+import numpy as np
 
 # Init SQLServer connection and get data
 Therabody = SQLServer('DbTherabody')
@@ -38,8 +39,18 @@ query = '''
 case = Therabody.select(query)
 # case.to_csv('case.csv')
 # case = pd.read_csv('case.csv')
+training_data = np.load('TF_intro.npz')
 
 # Set DataFrames
+
+# An exercise with the form y = 2x - 3z + 5
+# observations = 1000
+# xs = np.random.uniform(low=-10, high=10, size=(observations,1))
+# zs = np.random.uniform(-10, 10, (observations,1))
+# generated_inputs = np.column_stack((xs,zs))
+# noise = np.random.uniform(-1, 1, (observations,1))
+# generated_targets = 2*xs - 3*zs + 5 + noise
+# np.savez('TF_intro', inputs=generated_inputs, targets=generated_targets)
 
 # Init Regressions class
 deep_learning = DeepLearning()
